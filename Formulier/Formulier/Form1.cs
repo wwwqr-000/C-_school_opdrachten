64392b9d6dcc954509553e12826e880e3ce02bb9
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-03-07 09:33:01 +0100

    Update

--- Formulier/Formulier/Form1.cs
+++ Formulier/Formulier/Form1.cs
@@ -13,5 +13,9 @@ namespace Formulier {
         public Form1() {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
     }
 }
